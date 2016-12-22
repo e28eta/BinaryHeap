a69e50e36b04acb0376f6e8102630aa0e529c026
Dan Jackson <e28eta@yahoo.com>
2016-12-21 17:26:26 -0800

    Basic test of the heap, push & pop some elements

--- Tests/BinaryHeapTests/BinaryHeapTests.swift
+++ Tests/BinaryHeapTests/BinaryHeapTests.swift
@@ -1,17 +1,46 @@
 import XCTest
 @testable import BinaryHeap
 
+class Foo: Comparable, CustomStringConvertible {
+    let bar: Int
+
+    init(_ bar: Int) {
+        self.bar = bar
+    }
+
+    static func <(_ lhs: Foo, _ rhs: Foo) -> Bool {
+        return lhs.bar < rhs.bar
+    }
+
+    static func ==(_ lhs: Foo, _ rhs: Foo) -> Bool {
+        return lhs.bar == rhs.bar
+    }
+
+    var description: String {
+        return String(bar)
+    }
+}
+
 class BinaryHeapTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(BinaryHeap().text, "Hello, World!")
+    func testPopInSortedOrder() {
+        let h = BinaryHeap<Foo>()
+
+        let foos = [Foo(10), Foo(2), Foo(8), Foo(5)]
+        for foo in foos {
+            h.push(foo)
+        }
+
+        let sortedFoos = foos.sorted()
+
+        for foo in sortedFoos {
+            XCTAssertEqual(foo, h.pop(), "Heap should pop Foos in sorted order")
+        }
     }
 
 
     static var allTests : [(String, (BinaryHeapTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testPopInSortedOrder", testPopInSortedOrder),
         ]
     }
 }
